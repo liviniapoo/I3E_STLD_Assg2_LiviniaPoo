774d33ba6bb0a51c10c220277bd36612deaa66d7
liviniapoo <s10255969@connect.np.edu.sg>
2024-07-03 16:13:36 +0800

    Removed Editor

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,7 +8,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
+/*using UnityEditor.PackageManager;*/
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
